1ebcccc8837173f7b0aaccccae43bb8d3d9cf8d7
TQ0oS <0000xffff@gmail.com>
2019-05-18 15:58:50 +0300

    Creating a custom UICollectionViewCell

---
+++ Photorama/Photos/PhotoCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Bogdan Sasko on 5/18/19.
+//  Copyright © 2019 Vinso. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+    
+    func update(with image: UIImage?) {
+        if let imgToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imgToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
--- Photorama/Photos/PhotoDataSource.swift
+++ Photorama/Photos/PhotoDataSource.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class PhotoDataSource: NSObject, UICollectionViewDataSource {
     var photos = [Photo]()
-    let kImageCellName = "UICollectionViewCell"
+    let kImageCellName = "PhotoCollectionViewCell"
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kImageCellName, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kImageCellName, for: indexPath) as! PhotoCollectionViewCell
         return cell
     }
 }
